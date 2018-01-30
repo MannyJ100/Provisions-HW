2cc1b154664ef1e120729fc11c496bf1271f4602
Tech-A44 <tech-a44@Tech-A44s-MacBook-Air.local>
2018-01-30 10:46:03 +0000

    4 working

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 
 # install mongodb
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 # sudo apt-get install -y mongodb-org
@@ -16,10 +16,9 @@ sudo apt-get update
 # mongo --port 27017
 
 sudo apt-get install -y mongodb-org=3.2.18 mongodb-org-server=3.2.18 mongodb-org-shell=3.2.18 mongodb-org-mongos=3.2.18 mongodb-org-tools=3.2.18
-echo "mongodb-org hold" | sudo dpkg --set-selections
-echo "mongodb-org-server hold" | sudo dpkg --set-selections
-echo "mongodb-org-shell hold" | sudo dpkg --set-selections
-echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
-echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 sudo systemctl start mongod
 sudo systemctl enable mongod
+
+ sudo rm /etc/mongod.conf
+ sudo cp /home/ubuntu/db/mongod.conf /etc/mongod.conf
+ sudo service mongod restart
