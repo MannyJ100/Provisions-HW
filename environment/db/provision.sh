91dfc2de57b6f5aa8994787ce85e78d4afd24628
Tech-A44 <tech-a44@Tech-A44s-MacBook-Air.local>
2018-01-30 11:36:23 +0000

    cleaned up code

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -9,16 +9,12 @@ sudo apt-get install nginx -y
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
-# sudo apt-get install -y mongodb-org
-# sudo systemctl start mongod
-# sudo systemctl status mongodb
-# sudo systemctl enable mongodb
-# mongo --port 27017
 
 sudo apt-get install -y mongodb-org=3.2.18 mongodb-org-server=3.2.18 mongodb-org-shell=3.2.18 mongodb-org-mongos=3.2.18 mongodb-org-tools=3.2.18
 sudo systemctl start mongod
 sudo systemctl enable mongod
-
- sudo rm /etc/mongod.conf
- sudo cp /home/ubuntu/db/mongod.conf /etc/mongod.conf
- sudo service mongod restart
+# Removing file from guest
+sudo rm /etc/mongod.conf
+# Copying file from host to guest
+sudo cp /home/ubuntu/db/mongod.conf /etc/mongod.conf
+sudo service mongod restart
